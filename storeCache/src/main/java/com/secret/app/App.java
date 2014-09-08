45331ff3d07bcc618ca03f024814a50b9dc690d5
c0rtex <hassan.remi@gmail.com>
2014-09-08 18:34:31 +0200

    fir step cache store

---
+++ storeCache/src/main/java/com/secret/app/App.java
@@ -0,0 +1,19 @@
+package com.secret.app;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+import com.secret.app.Message.*;
+import com.secret.app.products.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main( String[] args ) {
+        final ActorSystem system = ActorSystem.create("system-name");
+        final ActorRef server = system.actorOf(Props.create(Server.class));
+         server.tell(new Start(), ActorRef.noSender());
+    }
+}
---
+++ storeCache/src/main/java/stores/ProductStoreImpl.java
@@ -0,0 +1,63 @@
+package stores;
+
+import com.google.common.base.Preconditions;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.secret.app.products.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.PostConstruct;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class ProductStoreImpl implements ProductStore
+{
+
+    private LoadingCache<String, Product> productCache = null;
+
+    @PostConstruct
+    public void init()
+    {
+        Integer itemDuration = 6;
+        TimeUnit itemDurationTimeUnit = TimeUnit.HOURS;
+
+        this.productCache = CacheBuilder.newBuilder()
+                .expireAfterWrite(itemDuration,itemDurationTimeUnit)
+                .build(new CacheLoader<String, Product>() {
+                            public Product load(String key){
+                                return innerGetProduct(key);
+                            }
+                        });
+    }
+
+    private Product innerGetProduct(String key) {
+        //Get en base
+        //Envoie akka
+        return null;
+    }
+
+    @Override
+    public Product getProduct(String productId)
+    {
+        Preconditions.checkNotNull(productId, "Param key must not be null");
+
+        Product product = null;
+
+        try {
+            product = this.productCache.get(productId);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        return product;
+    }
+
+    @Override
+    public void saveProduct(Product product)
+    {
+        Preconditions.checkNotNull(product, "Param product must not be null");
+        Preconditions.checkNotNull(product.getId(), "Param Id must not be null");
+
+        this.productCache.put(product.getId(), product);
+    }
+}
