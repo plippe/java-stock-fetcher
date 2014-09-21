7cd6a899571236c49eb2ad0e9b2cf61ea8d73874
Philippe Vinchon <p.vinchon@gmail.com>
2014-09-21 16:34:24 +0100

    moved store db, removed useless classes and kept boilerplate for db connection

--- storeDB/src/main/java/com/secret/app/old/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.secret.app.old;
-
-import com.secret.model.products.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product,String>{
-}
