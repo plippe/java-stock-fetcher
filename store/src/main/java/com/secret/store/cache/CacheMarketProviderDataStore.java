0e17c7300ee83210b294752e659f89fa1cc42b26
Philippe Vinchon <p.vinchon@gmail.com>
2014-09-28 13:14:04 +0100

    Guava would not work with the optional type making it hard to implement the cache with it, furthermore it doesn't support distributed caching, guava was replaced with a map (sorry remi)

---
+++ store/src/main/java/com/secret/store/cache/CacheMarketProviderDataStore.java
@@ -0,0 +1,21 @@
+package com.secret.store.cache;
+
+import java.util.Optional;
+import java.util.Map;
+import java.util.HashMap;
+
+import com.secret.store.generic.MarketProviderDataStore;
+import com.secret.model.marketprovider.MarketProviderData;
+
+public class CacheMarketProviderDataStore extends MarketProviderDataStore {
+
+  private Map<String, MarketProviderData> cache = new HashMap();
+
+  public Optional<MarketProviderData> findBySymbol(String symbol) throws Exception {
+    return cache.containsKey(symbol) ? Optional.of(cache.get(symbol)) : Optional.empty();
+  }
+
+  public void save(MarketProviderData value) throws Exception {
+    cache.put(value.getSymbol(), value);
+  }
+}
