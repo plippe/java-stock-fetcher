b1c3c38b2f2115a1f8c30ed3cf346ade5757c72c
Philippe Vinchon <p.vinchon@gmail.com>
2014-09-28 13:21:37 +0100

    tested the cache class

---
+++ store/src/test/java/com/secret/store/CacheMarketProviderDataStoreTest.java
@@ -0,0 +1,35 @@
+package com.secret.store.cache;
+
+import java.util.Optional;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import com.secret.model.marketprovider.MarketProviderData;
+
+public class CacheMarketProviderDataStoreTest extends TestCase {
+  public CacheMarketProviderDataStoreTest( String testName ) { super( testName ); }
+  public static Test suite() { return new TestSuite( CacheMarketProviderDataStoreTest.class ); }
+
+  public void testFindAndSave() {
+    try {
+      String symbol = "a";
+
+      CacheMarketProviderDataStore store = new CacheMarketProviderDataStore();
+      assertFalse(store.findBySymbol(symbol).isPresent());
+
+      MarketProviderData value1 = new MarketProviderData(symbol, 1.0, new java.util.Date(100, 1, 1, 1, 1));
+      store.save(value1);
+      Optional<MarketProviderData> found1 = store.findBySymbol(symbol);
+      assertTrue(found1.isPresent());
+      assertEquals(found1.get().getValue(), 1.0);
+
+      MarketProviderData value2 = new MarketProviderData(symbol, 2.0, new java.util.Date(100, 2, 2, 2, 2));
+      store.save(value2);
+      Optional<MarketProviderData> found2 = store.findBySymbol(symbol);
+      assertTrue(found2.isPresent());
+      assertEquals(found2.get().getValue(), 2.0);
+    } catch(Exception e) { fail("Should not raised errors in normal conditions"); }
+  }
+}
